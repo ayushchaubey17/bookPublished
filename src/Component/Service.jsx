630660761c36516209c7d6e98c100399daa37bd8
ayushchaubey17 <ayushreso1234@gmail.com>
2024-05-04 11:22:39 +0530

    new

--- src/Component/Service.jsx
+++ src/Component/Service.jsx
@@ -4,7 +4,7 @@ export default function Service() {
   return (
     <div className='space-y-4'>
         <h1 className="font-bold text-2xl text-center">Our Services</h1>
-      <img className='mx-auto w-11/12 rounded-md shadow-lg shadow-zinc-400 border-4 border-gray-300 ' src="a1.jpg" alt="" />
+      <img className='mx-auto w-11/12 rounded-md shadow-lg shadow-zinc-400 border-4 border-gray-300 ' src="../../public/a1.jpg" alt="" />
       <img className='mx-auto w-11/12 rounded-md shadow-lg shadow-zinc-400 border-4 border-gray-300 ' src="a2.jpg" alt="" />
       <img className='mx-auto w-11/12 rounded-md shadow-lg shadow-zinc-400 border-4 border-gray-300 ' src="a3.jpg" alt="" />
       <img className='mx-auto w-11/12 rounded-md shadow-lg shadow-zinc-400 border-4 border-gray-300 ' src="a4.jpg" alt="" />
